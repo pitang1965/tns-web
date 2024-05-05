a4ef974621aaac0000cf3c83e1a4651b6aac4820
pitang1965 <pitang1965@gmail.com>
2024-05-05 18:08:51 +0900

    💄 page.tsxの表示内容をすっきりさせ、ログイン時とそうでないときで表示を変えるようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+'use client';
 import Image from 'next/image';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
   return (
-    <div className='flex flex-col items-center justify-between p-24 bg-background text-foreground'>
+    <div className='flex flex-row items-center justify-between p-24 bg-background text-foreground'>
       <Image
         className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]'
         src='/over40.svg'
@@ -11,75 +14,10 @@ export default function Home() {
         height={37}
         priority
       />
-
-      <div className='grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left'>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className='mb-3 text-2xl font-semibold'>
-            Docs{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className='m-0 max-w-[30ch] text-sm opacity-50'>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className='mb-3 text-2xl font-semibold'>
-            Learn{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className='m-0 max-w-[30ch] text-sm opacity-50'>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className='mb-3 text-2xl font-semibold'>
-            Templates{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className='m-0 max-w-[30ch] text-sm opacity-50'>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className='mb-3 text-2xl font-semibold'>
-            Deploy{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className='m-0 max-w-[30ch] text-balance text-sm opacity-50'>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <p className='text-5xl py-4'>旅のしおり</p>
+        {!user && <p className='text-lg'>右上からログインしてください。</p>}
+        {user && <p className='text-lg'>こんにちは。{user.name}さん</p>}
       </div>
     </div>
   );
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -14,7 +14,7 @@ export function Header() {
     <div className='flex items-center justify-between text-foreground bg-white dark:bg-black p-2 gap-2'>
       <div className='flex-1'></div>
       <p className='absolute left-1/2 transform -translate-x-1/2 font-mono text-sm'>
-        Auth0による認証・認可の実験
+        Auth0による認証・認可の実験中...
       </p>
       <div className='flex items-center space-x-4'>
         {!user && <LoginButton />}
