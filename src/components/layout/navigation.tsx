6f961928f148159d3d33fcfa3f0a98aa59a30429
pitang1965 <pitang1965@gmail.com>
2024-05-05 20:57:25 +0900

    💄 ナビゲーションに現在のページ（ルート）を示すようにした

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,12 +1,40 @@
-// components/ui/navigation.tsx
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export function Navigation() {
+  const pathname = usePathname();
   return (
     <div className='flex items-center space-x-4'>
-      <Link href='/'>ホーム</Link>
-      <Link href='/profile'>プロフィール</Link>
-      <Link href='/help'>ヘルプ</Link>
+      <Link
+        href='/'
+        className={
+          pathname === '/'
+            ? 'underline decoration-1 underline-offset-4 decoration-current'
+            : ''
+        }
+      >
+        ホーム
+      </Link>
+      <Link
+        href='/profile'
+        className={
+          pathname === '/profile'
+            ? 'underline decoration-1 underline-offset-4 decoration-current'
+            : ''
+        }
+      >
+        プロフィール
+      </Link>
+      <Link
+        href='/help'
+        className={
+          pathname === '/help'
+            ? 'underline decoration-1 underline-offset-4 decoration-current'
+            : ''
+        }
+      >
+        ヘルプ
+      </Link>
     </div>
   );
 }
