b56a89c04f756082cff8fab8d680012fdd19ab0f
pitang1965 <pitang1965@gmail.com>
2024-08-07 22:08:19 +0900

    ✨ 旅程一覧ページに旅程詳細ページへのリンクを追加

--- src/components/itinerary/ItineraryCard.tsx
+++ src/components/itinerary/ItineraryCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { Itinerary } from '@/types';
 import {
   Card,
@@ -14,15 +15,19 @@ interface Props {
 
 export const ItineraryCard: React.FC<Props> = ({ itinerary }) => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{itinerary.title}</CardTitle>
-        <CardDescription>
-          {itinerary.startDate} - {itinerary.endDate}
-        </CardDescription>
-        <CardDescription>{itinerary.description}</CardDescription>
-      </CardHeader>
-      <CardContent>内容・・・</CardContent>
-    </Card>
+    <Link href={`/itineraries/${itinerary.id}`} passHref>
+      <Card className='cursor-pointer hover:shadow-lg transition-shadow duration-300'>
+        <CardHeader>
+          <CardTitle>
+            {itinerary.title}
+          </CardTitle>
+          <CardDescription>
+            {itinerary.startDate} - {itinerary.endDate}
+          </CardDescription>
+          <CardDescription>{itinerary.description}</CardDescription>
+        </CardHeader>
+        <CardContent>内容・・・</CardContent>
+      </Card>
+    </Link>
   );
 };
