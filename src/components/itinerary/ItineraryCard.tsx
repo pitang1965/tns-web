061565fee369d92707ee185ab882b0e95dd6d1e7
pitang1965 <pitang1965@gmail.com>
2024-08-11 23:25:35 +0900

    ♻️ TransportationBadge.tsxに分離

--- src/components/itinerary/ItineraryCard.tsx
+++ src/components/itinerary/ItineraryCard.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import { Itinerary } from '@/data/types/itinerary';
 import { Button } from '@/components/ui/button';
-import { Badge } from '@/components/ui/badge';
 import {
   Card,
   CardContent,
@@ -10,46 +9,16 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  TransportationBadge,
+  TransportationType,
+} from '@/components/TransportationBadge';
 
 type Props = {
   itinerary: Itinerary;
 };
 
-const transportationColorMap = {
-  CAR: 'bg-blue-500',
-  TRAIN: 'bg-green-500',
-  BUS: 'bg-yellow-500',
-  PLANE: 'bg-purple-500',
-  SHIP: 'bg-cyan-500',
-  OTHER: 'bg-gray-500',
-} as const;
-
-const transportationLabelMap = {
-  CAR: '自動車',
-  TRAIN: '鉄道',
-  BUS: 'バス',
-  PLANE: '飛行機',
-  SHIP: '船',
-  OTHER: 'その他',
-} as const;
-
-type TransportationType = keyof typeof transportationColorMap;
-
-const TransportationBadge: React.FC<{ type: TransportationType }> = ({
-  type,
-}) => {
-  const badgeColor = transportationColorMap[type] || 'bg-gray-500';
-  const label = transportationLabelMap[type] || 'その他';
-
-  return (
-    <Badge className={`${badgeColor} text-white self-start`}>{label}</Badge>
-  );
-};
-
 export const ItineraryCard: React.FC<Props> = ({ itinerary }) => {
-  const badgeColor =
-    transportationColorMap[itinerary.transportation.type] || 'bg-gray-500';
-
   return (
     <>
       <Card className='cursor-pointer hover:shadow-lg transition-shadow duration-300 p-2'>
