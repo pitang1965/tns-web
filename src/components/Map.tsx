77e458a15890335a358e4f93dc67d8fad587b5d5
pitang1965 <pitang1965@gmail.com>
2024-08-17 22:53:26 +0900

    🗾地図を日本語で表示するのにreact-map-glを使用しない方式に変更

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,28 +1,55 @@
-'use client';
-import { useState } from 'react';
-import MapGL, { Marker } from 'react-map-gl';
-
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Map = () => {
-  const [viewState, setViewState] = useState({
-    longitude: 139.7525, // 東京タワーの経度
-    latitude: 35.6586, // 東京タワーの緯度
-    zoom: 14,
-  });
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN || '';
 
-  return (
-    <div style={{ height: '400px', width: '100%' }}>
-      <MapGL
-        {...viewState}
-        onMove={(evt) => setViewState(evt.viewState)}
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-        mapStyle='mapbox://styles/mapbox/streets-v11'
-      >
-        <Marker longitude={139.7525} latitude={35.6586} color='red' />
-      </MapGL>
-    </div>
-  );
-}
+const MapComponent: React.FC = () => {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
 
-export default Map;
+  useEffect(() => {
+    if (mapContainer.current && !map.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [139.7525, 35.6586], // 東京の座標
+        zoom: 14,
+        language: 'ja', // 言語を日本語に設定
+        localIdeographFontFamily: "'Noto Sans', 'Noto Sans CJK JP', sans-serif",
+      });
+
+      map.current.on('load', () => {
+        if (map.current) {
+          // 日本語ラベルを設定
+          const layers = [
+            'country-label',
+            'state-label',
+            'settlement-label',
+            'poi-label',
+          ];
+          layers.forEach((layer) => {
+            if (map.current?.getLayer(layer)) {
+              map.current.setLayoutProperty(layer, 'text-field', [
+                'coalesce',
+                ['get', 'name_ja'],
+                ['get', 'name'],
+              ]);
+            }
+          });
+        }
+      });
+    }
+
+    return () => {
+      if (map.current) {
+        map.current.remove();
+        map.current = null;
+      }
+    };
+  }, []);
+
+  return <div ref={mapContainer} style={{ width: '100%', height: '400px' }} />;
+};
+
+export default MapComponent;
