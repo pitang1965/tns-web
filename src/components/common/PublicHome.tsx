82cc744f2b7f10c8fa15b1e15056c77a38ff8296
pitang1965 <pitang1965@gmail.com>
2025-04-03 22:15:07 +0900

    💄  PublicHomeコンポーネントのテキストを更新し、旅行計画の楽しさと利便性を強調

--- src/components/common/PublicHome.tsx
+++ src/components/common/PublicHome.tsx
@@ -12,7 +12,7 @@ const DynamicMap = dynamic(() => import('@/components/common/Map'), {
 export default function ClientHome() {
   return (
     <div className='flex flex-col items-center justify-center p-4 sm:p-8 md:p-12 lg:p-24 bg-background text-foreground'>
-      <H1>旅のしおり作成</H1>
+      <H1>『旅のしおり』について</H1>
       <Image
         className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] mb-4'
         src='/azuki.webp'
@@ -22,10 +22,13 @@ export default function ClientHome() {
         priority
       />
       <div>
-        <LargeText>旅行を計画するにはログインしてください。</LargeText>
+        <LargeText>旅行計画をもっと簡単に、もっと楽しく。</LargeText>
         <LargeText>
-          旅のしおりを使って、あなたの旅行をより楽しく、効率的に計画しましょう。
+        Google Mapsと連携して旅程を作成・管理できるWebアプリです。目的地の座標をクリップボード経由で簡単に登録し、スムーズな経路探索が可能。日帰りから複数日の旅行まで対応し、作成した旅程は他の人と共有することもできます。
         </LargeText>
+        <LargeText>旅のストレスを減らし、思い出に残る体験をサポートします。</LargeText>
+        <LargeText>※旅程を編集するにはログインが必要です。</LargeText>
+        <LargeText>※現在開発中のサービスです。</LargeText>
         <SmallText>
           続行することにより、本アプリの利用規約及びプライバシー及びCookieに関する声明に同意するものとします。
         </SmallText>
