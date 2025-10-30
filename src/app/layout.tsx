28abfd4d0345a1b31d58d28ef595f59c74c26b1d
pitang1965 <pitang1965@gmail.com>
2025-10-30 12:57:38 +0900

    💄 ビューポート設定を変更し、最大スケールを1に制限し、ユーザーのスケーラブルを無効化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,8 @@ const inter = Inter({ subsets: ['latin'] });
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 5,
-  userScalable: true,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export const metadata: Metadata = {
