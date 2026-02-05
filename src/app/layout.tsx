09ece14439647cc82d1efb3ffd34efc50b17a237
pitang1965 <pitang1965@gmail.com>
2026-02-05 09:38:36 +0900

    🐛 AdSenseスクリプトのタグを修正し、戦略属性を削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,11 +98,10 @@ export default function RootLayout({
           </>
         )}
         {process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID && (
-          <Script
+          <script
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
             crossOrigin='anonymous'
-            strategy='afterInteractive'
           />
         )}
         <ErrorBoundary>
